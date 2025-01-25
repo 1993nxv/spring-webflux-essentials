acf7e8fd1bf2387a2b9c7f390aa34c044409b2b1
Delmondes <wellington.apk@gmail.com>
2025-01-24 23:42:46 -0300

    SwitchIfEmpty and ResponseStatusException

--- src/main/java/academy/devdojo/webflux/controller/AnimeController.java
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -4,10 +4,12 @@ import academy.devdojo.webflux.domain.Anime;
 import academy.devdojo.webflux.service.AnimeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,7 +28,9 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Mono<Anime> findById(@PathVariable int id){
-        return animeService.findById(id);
+        return animeService.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime não encontrado")))
+                .log();
     }
 
 }
