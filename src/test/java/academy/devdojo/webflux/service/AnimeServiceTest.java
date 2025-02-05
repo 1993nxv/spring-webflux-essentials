9f6b37961f6cb018d57ec101016aebc7cbc5f954
Delmondes <wellington.apk@gmail.com>
2025-02-04 23:46:27 -0300

    Estrutura para testes blockhound

--- src/test/java/academy/devdojo/webflux/service/AnimeServiceTest.java
+++ src/test/java/academy/devdojo/webflux/service/AnimeServiceTest.java
@@ -1,7 +1,29 @@
 package academy.devdojo.webflux.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import academy.devdojo.webflux.domain.Anime;
+import academy.devdojo.webflux.repository.AnimeRepository;
+import academy.devdojo.webflux.util.AnimeCreator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.blockhound.BlockHound;
+
+@ExtendWith(SpringExtension.class)
 class AnimeServiceTest {
 
+    @InjectMocks
+    private AnimeService animeService;
+
+    @Mock
+    private AnimeRepository animeRepository;
+
+    private final Anime anime = AnimeCreator.createValidAnime();
+
+    @BeforeAll
+    public static void blockHoundSetup(){
+        BlockHound.install();
+    }
 }
